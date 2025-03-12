05af4488965b030805ff0d1eb814910ac32142e4
francis1009 <francism56789@gmail.com>
2025-03-12 13:24:38 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
+import { cloudflare } from "unenv";
 
 // https://vitejs.dev/config/
 export default defineConfig({
