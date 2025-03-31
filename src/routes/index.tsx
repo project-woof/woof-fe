50006ec622c27906d40cbac4c13934742782944a
ryanpxp <ryanpxp@outlook.com>
2025-03-31 19:40:34 +0800

    remove unused import

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { PetsitterCard } from "@/components/index/PetsitterCard";
 import { Button } from "@/components/ui/button";
 import { Filter } from "lucide-react";
