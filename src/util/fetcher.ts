4ab9599caa47d956e1cb7ea6d7d44c75afbefd36
francis1009 <francism56789@gmail.com>
2025-04-02 03:50:59 +0800

    Update fetcher

--- src/util/fetcher.ts
+++ src/util/fetcher.ts
@@ -1,27 +1,23 @@
 import ky, { HTTPError, type KyResponse } from "ky";
 import type { Options } from "node_modules/ky/distribution/types/options";
 
-const api = ky.create({
-  prefixUrl: import.meta.env.VITE_API_URL,
-  credentials: "include",
-  hooks: {
-    beforeRequest: [
-      (request) => {
-        const token = localStorage.getItem("bearer_token");
-        if (token) {
-          request.headers.set("Authorization", `Bearer ${token}`);
-        }
-      },
-    ],
-  },
-});
-
 export async function fetcher(
   endpoint: string,
   options: Options = {}
 ): Promise<KyResponse> {
   try {
-    const res = await api(endpoint, options);
+    const token = localStorage.getItem("bearer_token");
+    let headers: Headers;
+    if (options.headers instanceof Headers) {
+      headers = options.headers;
+    } else {
+      headers = new Headers();
+    }
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    options.headers = headers;
+    const res = await ky(import.meta.env.VITE_API_URL + endpoint, options);
     return res;
   } catch (err: any) {
     const error = err as HTTPError;
